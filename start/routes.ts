b115a4a0bb1648bf7410e57f566e1f58c3223ea7
Luis Coelho <mancoeluis@gmail.com>
2022-08-18 16:34:35 -0300

    🗃️ config mysql and healthcheck

--- start/routes.ts
+++ start/routes.ts
@@ -19,7 +19,22 @@
 */
 
 import Route from '@ioc:Adonis/Core/Route'
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Database from '@ioc:Adonis/Lucid/Database'
 
 Route.get('/', async () => {
   return { hello: 'world' }
+})
+
+Route.get('/db_connection', async ({ response }: HttpContextContract) => {
+  await Database.report().then((health) => {
+    if (health.health.healthy === true) {
+      return response.ok({ message: `Awesome! Connection is healthy (:` })
+    }
+    return response.status(500).json({ message: `Connection is not healthy :(` })
+  })
+})
+
+Route.get('/users', async () => {
+  return { message: `Returns all users.` }
 })
