69e69530b93b5d589a9cd47ee0568544f6912e4f
Luis Coelho <mancoeluis@gmail.com>
2022-08-20 12:26:02 -0300

    ✅ add login in db test

--- tests/functional/database.spec.ts
+++ tests/functional/database.spec.ts
@@ -1,8 +1,10 @@
 import { test } from '@japa/runner'
+import User from 'App/Models/User'
 
 test.group('Database', () => {
   test('DB Connection', async ({ client }) => {
-    const response = await client.get('/db_connection')
+    const adminUser = await User.findOrFail(1)
+    const response = await client.get('/tests/db_connection').guard('api').loginAs(adminUser)
 
     response.assertStatus(200)
     response.assertBodyContains({ message: `Awesome! Connection is healthy (:` })
